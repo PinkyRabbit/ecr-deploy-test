8f7d137ae17b7b2bb5760189b86be85467c1fc46
Mikita Melnikau <usesa@yandex.com>
2020-12-28 12:36:32 +0300

    switch to 3000 port, ECS test config added

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs');
